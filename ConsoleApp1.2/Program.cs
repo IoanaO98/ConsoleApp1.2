eb378b367f5272d366b2294d20856c20495b4781
IoanaO98 <osan.ioana1998@gmail.com>
2022-10-11 14:38:38 +0300

    methods

--- ConsoleApp1.2/Program.cs
+++ ConsoleApp1.2/Program.cs
@@ -12,14 +12,21 @@ namespace Exercitii
         static void Main(string[] args)
         {
             //1
+            string characterName = "Ana";
+            int characterAge;
+            characterAge = 20;
+            Console.WriteLine("There once was a girl named " + characterName);
+            Console.WriteLine("She was " + characterAge + "years old");
+            Console.WriteLine("She really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            //2
+
             var names = new List<string> { "ioana", "Ana", "Fiona" };
             foreach (var name in names)
             {
                 Console.WriteLine($"hello {name.ToUpper()}!");
             }
-
-            //2
-            Console.WriteLine();
             names.Add("Maria");
             names.Add("Ion");
             names.Remove("Ana");
@@ -29,51 +36,69 @@ namespace Exercitii
             }
             Console.WriteLine($"My name is {names[0]}.");
             Console.WriteLine($" I've added {names[2]} and {names[3]} to the list.");
-            //3
-            Console.WriteLine($"The list has {names.Count} people in it");
+
             //4
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+            //5
             var index = names.IndexOf("Ioana");
+
+            //3
+            Console.WriteLine();
             if (index != -1)
                 Console.WriteLine($"The name {names[index]} is at index {index}");
 
             var notfound = names.IndexOf("Not Found");
             Console.WriteLine($"Then an item is not found, IndexOf returns {notfound}");
-            //5
+
+            //6
             names.Sort();
             foreach (var name in names)
             {
                 Console.WriteLine($"hello {name.ToUpper()}!");
             }
-            //6
+
+            //7
             var fibonacciNumbers = new List<int> { 1, 1 };
             var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
             var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
             fibonacciNumbers.Add(previous + previous2);
             foreach (var item in fibonacciNumbers)
                 Console.WriteLine(item);
+            //8
+            string sentence = "Maria are mere";
+
+            Console.WriteLine($"The sentence {sentence} has the {sentence.Length} letters ");
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            fibonaciiNumbers(cars);
+            Console.ReadLine();
         }
 
         static void fibonaciiNumbers(string[] args)
         {
-            var fibonacciNumbers = new List<int> { 1, 1 };
-            while (fibonacciNumbers.Count < 20)
+            List<int> fibonaciiNumbers = new List<int> { 1, 1 };
+            while (fibonaciiNumbers.Count < 20)
             {
-                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
-                fibonacciNumbers.Add(previous + previous2);
+                int previous = fibonaciiNumbers[fibonaciiNumbers.Count - 1];
+                int previous2 = fibonaciiNumbers[fibonaciiNumbers.Count - 2];
+                fibonaciiNumbers.Add(item: previous + previous2);
             }
-            foreach (var item in fibonacciNumbers)
+            foreach (int item in fibonaciiNumbers)
                 Console.WriteLine(item);
         }
 
-        static void sentencelength(string[] args)
+        static void sentencelength(int[] args)
+
         {
             //2. Write a program in C# Sharp to find the length of a string without using library function
 
             string sentence = "Maria are mere";
 
             Console.WriteLine($"The sentence {sentence} has the {sentence.Length} letters ");
+            Console.ReadLine();
         }
+
+
     }
 
 }
