5d7d847ffbb046775b1b7c7b38fab31aa384e6bf
IoanaO98 <osan.ioana1998@gmail.com>
2022-10-12 14:42:15 +0300

    Methods

--- ConsoleApp1.2/Program.cs
+++ ConsoleApp1.2/Program.cs
@@ -7,119 +7,133 @@ using System.Threading.Tasks;
 
 namespace Exercitii
 { }
-    internal class Program
+internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //1
+        string characterName = "Ana";
+        int characterAge;
+        characterAge = 20;
+        Console.WriteLine("There once was a girl named " + characterName);
+        Console.WriteLine("She was " + characterAge + "years old");
+        Console.WriteLine("She really liked the name " + characterName);
+        Console.WriteLine("But didn't like being " + characterAge);
+
+        //2
+
+        var names = new List<string> { "ioana", "Ana", "Fiona" };
+        foreach (var name in names)
         {
-            //1
-            string characterName = "Ana";
-            int characterAge;
-            characterAge = 20;
-            Console.WriteLine("There once was a girl named " + characterName);
-            Console.WriteLine("She was " + characterAge + "years old");
-            Console.WriteLine("She really liked the name " + characterName);
-            Console.WriteLine("But didn't like being " + characterAge);
+            Console.WriteLine($"hello {name.ToUpper()}!");
+        }
+        names.Add("Maria");
+        names.Add("Ion");
+        names.Remove("Ana");
+        foreach (var name in names)
+        {
+            Console.WriteLine($"Hello {name.ToUpper()}!");
+        }
+        Console.WriteLine($"My name is {names[0]}.");
+        Console.WriteLine($" I've added {names[2]} and {names[3]} to the list.");
 
-            //2
+        //4
+        Console.WriteLine($"The list has {names.Count} people in it");
 
-            var names = new List<string> { "ioana", "Ana", "Fiona" };
-            foreach (var name in names)
-            {
-                Console.WriteLine($"hello {name.ToUpper()}!");
-            }
-            names.Add("Maria");
-            names.Add("Ion");
-            names.Remove("Ana");
-            foreach (var name in names)
-            {
-                Console.WriteLine($"Hello {name.ToUpper()}!");
-            }
-            Console.WriteLine($"My name is {names[0]}.");
-            Console.WriteLine($" I've added {names[2]} and {names[3]} to the list.");
+        //5
+        var index = names.IndexOf("Ioana");
 
-            //4
-            Console.WriteLine($"The list has {names.Count} people in it");
+        //3
+        Console.WriteLine();
+        if (index != -1)
+            Console.WriteLine($"The name {names[index]} is at index {index}");
 
-            //5
-            var index = names.IndexOf("Ioana");
+        var notfound = names.IndexOf("Not Found");
+        Console.WriteLine($"Then an item is not found, IndexOf returns {notfound}");
 
-            //3
-            Console.WriteLine();
-            if (index != -1)
-                Console.WriteLine($"The name {names[index]} is at index {index}");
-
-            var notfound = names.IndexOf("Not Found");
-            Console.WriteLine($"Then an item is not found, IndexOf returns {notfound}");
-
-            //6
-            names.Sort();
-            foreach (var name in names)
-            {
-                Console.WriteLine($"hello {name.ToUpper()}!");
-            }
-
-            //7
-            var fibonacciNumbers = new List<int> { 1, 1 };
-            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
-            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
-            fibonacciNumbers.Add(previous + previous2);
-            foreach (var item in fibonacciNumbers)
-                Console.WriteLine(item);
-            //8
-
-            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
-            fibonaciiNumbers(cars);
-           
-
-            string[] ceva = { "1", "2", "3" };
-            sentencelength(ceva);
-
-            string[] nume = { "Ioana", "Paula", "Bunica" };
-            enterName(nume);
-
-            string[] numere = { "1", "2", "3" };
-            convers(numere);
-
-            Console.ReadLine();
+        //6
+        names.Sort();
+        foreach (var name in names)
+        {
+            Console.WriteLine($"hello {name.ToUpper()}!");
         }
 
-        static void fibonaciiNumbers(string[] args)
-        {
-            List<int> fibonaciiNumbers = new List<int> { 1, 1 };
-            while (fibonaciiNumbers.Count < 20)
-            {
-                int previous = fibonaciiNumbers[fibonaciiNumbers.Count - 1];
-                int previous2 = fibonaciiNumbers[fibonaciiNumbers.Count - 2];
-                fibonaciiNumbers.Add(item: previous + previous2);
-            }
-            foreach (int item in fibonaciiNumbers)
-                Console.WriteLine(item);
-        }
+        //7
+        var fibonacciNumbers = new List<int> { 1, 1 };
+        var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+        var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+        fibonacciNumbers.Add(previous + previous2);
+        foreach (var item in fibonacciNumbers)
+            Console.WriteLine(item);
+        //8
 
-        static void sentencelength(string[] args)
+        string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+        fibonaciiNumbers(cars);
 
-        {
-            //2. Write a program in C# Sharp to find the length of a string without using library function
 
-            string sentence = "Maria are mere";
+        string[] ceva = { "1", "2", "3" };
+        sentencelength(ceva);
 
-            Console.WriteLine($"The sentence {sentence} has the {sentence.Length} letters ");
-            Console.ReadLine();
-        }
-        static void enterName(string[] args)
-        {
-            Console.WriteLine("Enter your name: ");
-            string name  = Console.ReadLine();
-            Console.WriteLine("Enter your age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Hello " + name + " you are " + age);
-         
-        }
-        static void convers(string[] args)
-        {
-        int num = Convert.ToInt32("ndja");
-        Console.WriteLine(num + 6);
-        }
+        string[] nume = { "Ioana", "Paula", "Bunica" };
+        enterName(nume);
+
+        string[] numere = { "1", "2", "3" };
+        ConversNumber(numere);
+
+        Console.ReadLine();
     }
+
+    static void fibonaciiNumbers(string[] args)
+    {
+        List<int> fibonaciiNumbers = new List<int> { 1, 1 };
+        while (fibonaciiNumbers.Count < 20)
+        {
+            int previous = fibonaciiNumbers[fibonaciiNumbers.Count - 1];
+            int previous2 = fibonaciiNumbers[fibonaciiNumbers.Count - 2];
+            fibonaciiNumbers.Add(item: previous + previous2);
+        }
+        foreach (int item in fibonaciiNumbers)
+            Console.WriteLine(item);
+    }
+
+    static void sentencelength(string[] args)
+
+    {
+        //2. Write a program in C# Sharp to find the length of a string without using library function
+
+        string sentence = "Maria are mere";
+
+        Console.WriteLine($"The sentence {sentence} has the {sentence.Length} letters ");
+        Console.ReadLine();
+    }
+    static void enterName(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter your age: ");
+        string age = Console.ReadLine();
+        Console.WriteLine("Hello " + name + " you are " + age);
+
+    }
+    static void ConversNumber(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(num1 + num2);
+
+        //decimal
+        Console.Write("Enter a decimal number: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter another decimal number: ");
+        double num4 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine(num3 + num4);
+        Console.ReadLine();
+    }
+    
+}   
 
 
